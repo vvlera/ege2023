a5876746c6d71244f04ea6cc1e3cbcd18db4e18a
Уйманова Валерия <114632272+vvlera@users.noreply.github.com>
2022-12-12 16:17:45 +0700

    Add files via upload

--- 9 задание.py
+++ 9 задание.py
@@ -6,11 +6,28 @@ with open('C:/Users/student/Documents/142Б/9.txt') as f:
     #print(s)
     sp=list(map(int,s))
     #print(sp)
-    
-    for a in sp:
-        res6=[]
-        res6= sp[0:6]
+    x=y=0
+    for a in range(len(sp)):
+        x=y
+        y=x+6
+        res6=sp[x:y]
         print(res6)
+        #input()
+        
+        for q in res6:
+            if res6.count(q)==2:
+                b = q                
+                del res6[res6.index(q)]
+                del res6[res6.index(q)]                
+                print(res6)
+                input()
+                
+            
+    
+        
+        
+            
+        
         
         
     
