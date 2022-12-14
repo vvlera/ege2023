32f98c17dc8bd0fb88ac867e9b402b812b94a35b
Уйманова Валерия <114632272+vvlera@users.noreply.github.com>
2022-12-15 00:05:50 +0700

    Add files via upload

---
+++ 9 задание.py
@@ -0,0 +1,48 @@
+with open('C:/Users/student/Documents/142Б/9.txt') as f:
+    s=f.readlines()
+    #print(s)
+    for i in range (len(s)):
+        s[i]=s[i].replace(';',' ')
+    #print(s)
+    sp=list(map(int,s))
+    #print(sp)
+    x=y=0
+    for a in range(len(sp)):
+        x=y
+        y=x+6
+        res6=sp[x:y]
+        print(res6)
+        #input()
+        
+        for q in res6:
+            if res6.count(q)==2:
+                b = q                
+                del res6[res6.index(q)]
+                del res6[res6.index(q)]                
+                #print(res6)
+                otv=0
+                if len(res6)==4:
+                    if sum(res6[q])/len(res6)<=b*2:
+                        otv+=1
+                    else:
+                        del res6
+
+                else:
+                    del res6  
+            elif res6.count(q)>=2:
+                del res6
+            print(otv)
+                
+            
+    
+        
+        
+            
+        
+        
+        
+    
+    
+    
+
+
