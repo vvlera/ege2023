9f37bac25a29876b08868baffd160e45c5e0c77f
Уйманова Валерия <114632272+vvlera@users.noreply.github.com>
2023-01-12 12:10:05 +0700

    Add files via upload

---
+++ 8дист.py
@@ -0,0 +1,34 @@
+'''from itertools import product
+nums=list(product('345678',repeat=3))
+print(nums)'''
+
+'''from itertools import product
+nums=product('01234567',repeat=5)
+k=0
+n='16 36 56 76 61 63 65 67'
+nn=n.split()
+#print(nn)
+for n in nums:
+    numb=''.join(n)
+    #print(numb)
+    sp=[]
+    if numb.count('6')==1 and numb[0]!='0':
+        for x in nn:
+            if x in numb:
+                sp.append(x)
+        if not sp: 
+            k+=1
+print(k)
+
+'''
+for i in range(1,100):
+    chislo=''
+    num=(bin(i)[2:])
+    if num.count('1')%2==0:
+        chislo='10'+num[2:]+'0'
+
+    if num.count('1')%2!=0:
+        chislo='11'+num[2:]+'1'
+    if int(chislo,2)>40:
+        print (i, int(chislo,2))
+        break
